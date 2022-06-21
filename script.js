f33f264444aaf0a473cb3adb862f609f233666f4
Nicholas Jones <nicholas.a.jones@live.co.ukm>
2022-06-21 21:21:34 +0100

    added comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let pwLength;
+// array of random alphanumerics
 const random = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 let rtnPw = []
+// returns queries
 const qs = (query) => {
     return document.querySelector(query);
 }
@@ -9,6 +11,7 @@ const qs = (query) => {
 const btn = qs('.btn');
 const password = qs('.password');
 
+// takes input size password length
 document.querySelector('#passwordLength').addEventListener('keyup', (e) => {
     pwLength = e.target.value
 })
