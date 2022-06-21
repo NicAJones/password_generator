0fa1fea2dfc1f59be64b4ff2ee752305e96b2c75
Nicholas Jones <nicholas.a.jones@live.co.ukm>
2022-06-21 21:14:02 +0100

    added password variable length feature

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const pwLength = 10;
+let pwLength;
 const random = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 let rtnPw = []
 const qs = (query) => {
     return document.querySelector(query);
 }
 
+
 const btn = qs('.btn');
 const password = qs('.password');
 
+document.querySelector('#passwordLength').addEventListener('keyup', (e) => {
+    pwLength = e.target.value
+})
 
 
 btn.addEventListener('click', (e) => {
