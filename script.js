b2df1473e55293a3595a8f14023c931b1fa1c454
Nicholas Jones <nicholas.a.jones@live.co.uk>
2022-06-26 19:09:36 +0100

    added clear button, added styling, removed numbber input scrolling

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ let pwLength;
 const random = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 let rtnPw = []
 // returns queries
-const qs = (query) => {
+let qs = (query) => {
     return document.querySelector(query);
 }
 
 
-const btn = qs('.btn');
-const password = qs('.password');
+let btn = qs('.btn');
+let password = qs('.password');
+let clear = qs('.clear');
+
+
 
 // takes input size password length.
 document.querySelector('#passwordLength').addEventListener('keyup', (e) => {
@@ -24,3 +27,4 @@ btn.addEventListener('click', (e) => {
     }
     password.textContent = rtnPw.join().replaceAll(',', '');
 })
+
